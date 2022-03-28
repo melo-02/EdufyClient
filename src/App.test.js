20dad2fa07a623da6b5bf48b92fbc485720380f3
[ripeliz] <elizabethchoy02@gmail.com>
2022-03-28 18:54:16 -0400

    3 test cases, rendering the 3 pages

--- src/App.test.js
+++ src/App.test.js
@@ -2,9 +2,25 @@
 import {render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Login from './Pages/Login';
+import Footer from './Pages/Footer';
+import Home from './Pages/Home';
+import Navigation from './Pages/Navigation';
 
-test ('renders link to login', async () => {
+test ('renders registration page', async () => {
     render(<Login/>);
     const loginElement = screen.getByText(/User Login/);
     expect(loginElement).toBeInTheDocument();
 });
+
+test ('renders footer', async () => {
+    render(<Footer/>);
+    const footerElement = screen.getByText(/Edufy 2022/);
+    expect(footerElement).toBeInTheDocument();
+});
+
+test ('renders homepage', async () => {
+    render(<Home/>);
+    const homeElement = screen.getByText(/Home/);
+    expect(homeElement).toBeInTheDocument();
+});
+
