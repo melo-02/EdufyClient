d12cad33de3d24640c7c26697b509254b95f2ba9
[ripeliz] <elizabethchoy02@gmail.com>
2022-04-09 19:49:19 -0400

    added /classes page

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -3,3 +3,4 @@ export { default as Footer } from "./Footer";
 export { default as Home } from "./Home";
 export { default as Login } from "./Login";
 export { default as About } from "./About";
+export { default as Classes } from "./Classes";
