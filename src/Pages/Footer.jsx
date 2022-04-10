10b7121bb21287603d7488d0fff80efa993d6c6e
[ripeliz] <elizabethchoy02@gmail.com>
2022-04-09 20:51:16 -0400

    new sign up button + fixed footer

--- src/Pages/Footer.jsx
+++ src/Pages/Footer.jsx
@@ -2,16 +2,24 @@ import React from "react";
 
 function Footer() {
   return (
-    <div className="footer">
-      <footer class="py-5 bg-dark fixed-bottom">
-        <div class="container">
-          <p class="m-0 text-center text-white">
-            Copyright &copy; Edufy 2022
-          </p>
+    
+
+
+      <footer class="pt-3 pt-md-4 bg-dark border-top fixed-bottom">
+        <div class="row">
+          <div class="col-12 col-md">
+           
+            <small class="d-block mb- text-white">&copy; Edufy 2022</small>
+          </div>
+
         </div>
       </footer>
-    </div>
-  );
-}
+
+
+
+   
+
+
+  )}
 
 export default Footer;
